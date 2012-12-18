da30fc50a188eb6651e8f552980d64330c7d4eaa
dhilton <dan@lightsandcolors.com>
2012-12-17 21:47:23 -0700

    Fix migration, add fields to edit form

--- db/migrate/20121218042230_add_title_description_to_publinator_publications.rb
+++ db/migrate/20121218042230_add_title_description_to_publinator_publications.rb
@@ -1,6 +1,6 @@
 class AddTitleDescriptionToPublinatorPublications < ActiveRecord::Migration
   def change
     add_column :publinator_publications, :title_tag, :string
-    add_column :publinator_publications, :meta_description, :string
+    add_column :publinator_publications, :meta_description, :text
   end
 end
