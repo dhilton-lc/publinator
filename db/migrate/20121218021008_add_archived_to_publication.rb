87b9425be458f37d4d90e8dd9d1b902ddb1e6f46
dhilton <dan@lightsandcolors.com>
2012-12-17 20:15:22 -0700

    Modify migration: make 'archived' non-nullable

--- db/migrate/20121218021008_add_archived_to_publication.rb
+++ db/migrate/20121218021008_add_archived_to_publication.rb
@@ -1,5 +1,5 @@
 class AddArchivedToPublication < ActiveRecord::Migration
   def change
-    add_column :publinator_publications, :archived, :boolean
+    add_column :publinator_publications, :archived, :boolean, :null => false, :default => false
   end
 end
